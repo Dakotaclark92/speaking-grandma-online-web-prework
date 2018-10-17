1cf823d14f28baa4a99ef272fc1179690af65ff6
Dakota Clark <bannaray95@gmail.com>
2018-10-17 03:25:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 IF speak_to_grandma == "I love you Grandma!".upcase
-puts "I LOVE YOU TOO PUMPKIN!"
+puts "I love you too pumpkin!".upcase
 end
 
 IF speak_to_grandma == "Hi Nana, how are you?"
-puts "HUH?! SPEAK UP, SONNY!"
+puts "Huh?! speak up, sonny!".upcase
 elsif speak_to_grandma "Hi!"
-puts "HUH?! SPEAK UP, SONNY!"
+puts "Huh?! speak up, sonny!".upcase
 elsif speak_to_grandma "what did you eat today".upcase
+puts "No, not since 1938!".upcase
