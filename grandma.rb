405c7a9d9876dd02c9b6fcbdc187e806599857ac
Dakota Clark <bannaray95@gmail.com>
2018-10-17 03:33:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,10 @@ end
 
 IF speak_to_grandma == "Hi Nana, how are you?"
 puts "Huh?! speak up, sonny!".upcase
-elsif speak_to_grandma "Hi!"
+elsif speak_to_grandma == "Hi!"
 puts "Huh?! speak up, sonny!".upcase
 elsif speak_to_grandma "what did you eat today".upcase
 puts "No, not since 1938!".upcase
-elsif "What?".upcase
-puts 
+elsif speak == "What?".upcase
+puts "No, not since 1938!".upcase
+end
